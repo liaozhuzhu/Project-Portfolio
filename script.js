80d201806d5d493a701dcd4d2fc94e58634c02a2
Liao Zhu <liao.zhu.cs@gmail.com>
2022-06-12 03:11:13 -0500

    Finished Mobile Menu Bar

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const navSlide = () => {
   const menu = document.querySelector(".menu");
   const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
   menu.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+
+    // Menu Animation
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      }
+      else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
+      }
+    });
+
+    //Menu Animation After Clicked
+    menu.classList.toggle("toggle");
+
   });
+
 }
 
 navSlide();
